345d6ee16b2e99c45948b8c48de301ea93d14974
zmad5306 <zachary.maddox@gmail.com>
2018-03-20 17:23:19 -0400

    init project

---
+++ src/test/java/com/example/gl/dept/DeptApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.gl.dept;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeptApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
