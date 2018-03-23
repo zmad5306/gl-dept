41ae79312d09530402d58318a69a0d6c0f15ee9e
zmad5306 <zachary.maddox@gmail.com>
2018-03-23 06:21:53 -0400

    converted to mongo in the k8s cluster

--- src/main/java/com/example/gl/dept/service/DepartmentService.java
+++ src/main/java/com/example/gl/dept/service/DepartmentService.java
@@ -1,9 +1,14 @@
 package com.example.gl.dept.service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,18 +19,22 @@ import com.example.gl.dept.repository.DepartmentRepository;
 @Service
 public class DepartmentService {
 	
+	private String[] defaultDeptNames = {"Frozen", "Dairy", "Bread", "Canned"};
+	private final Logger logger = LoggerFactory.getLogger(DepartmentService.class);
+	
 	@Autowired
 	private DepartmentRepository repo;
 	
-//	@PostConstruct
-//	public void init() {
-//		Department dept1 = new Department("Frozen");
-//		Department dept2 = new Department("Dairy");
-//		Department dept3 = new Department("Bread");
-//		repo.insert(dept1);
-//		repo.insert(dept2);
-//		repo.insert(dept3);
-//	}
+	@PostConstruct
+	public void init() {
+		List<String> deptNames = repo.findAll().stream().map(dept -> dept.getName()).collect(Collectors.toList());
+		Arrays.asList(defaultDeptNames).stream().filter(deptName -> !deptNames.contains(deptName)).forEach(deptName -> {
+			logger.info("Creating dept: " + deptName);
+			Department dept = new Department(deptName);
+			repo.insert(dept);
+		});
+		
+	}
 	
 	public List<DepartmentDto> getAllDepartments() {
 		return repo.findAll().stream().map(dept -> new DepartmentDto(dept)).collect(Collectors.toList());
