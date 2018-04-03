d6fd58f35b48f374efe2697bcf92316fe080909e
zmad5306 <zachary.maddox@gmail.com>
2018-04-02 20:32:07 -0400

    removed defalt depts

--- src/main/java/com/example/gl/dept/service/DepartmentService.java
+++ src/main/java/com/example/gl/dept/service/DepartmentService.java
@@ -1,18 +1,11 @@
 package com.example.gl.dept.service;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import javax.annotation.PostConstruct;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
 
 import com.example.gl.dept.dto.DepartmentDto;
 import com.example.gl.dept.model.Department;
@@ -21,24 +14,8 @@ import com.example.gl.dept.repository.DepartmentRepository;
 @Service
 public class DepartmentService {
 	
-	@Value("${default-dept-names}")
-	private String defaultDeptNames;
-	private final Logger logger = LoggerFactory.getLogger(DepartmentService.class);
-	
 	@Autowired
 	private DepartmentRepository repo;
-	
-	@PostConstruct
-	public void init() {
-		Assert.notNull(defaultDeptNames, "defaultDeptNames must be provided in dept-config configmap");
-		List<String> deptNames = repo.findAll().stream().map(dept -> dept.getName()).collect(Collectors.toList());
-		Arrays.asList(defaultDeptNames.split(",")).stream().filter(deptName -> !deptNames.contains(deptName)).forEach(deptName -> {
-			logger.info("Creating dept: " + deptName);
-			Department dept = new Department(deptName);
-			repo.insert(dept);
-		});
-		
-	}
 	
 	public List<DepartmentDto> getAllDepartments() {
 		return repo.findAll().stream().map(dept -> new DepartmentDto(dept)).collect(Collectors.toList());
